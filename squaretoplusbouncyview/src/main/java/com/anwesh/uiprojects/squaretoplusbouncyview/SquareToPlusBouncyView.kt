d85c433a9c416620af9d027f610a65c991d09db8
Anwesh43 <anweshthecool0@gmail.com>
2019-11-24 18:31:58 +0530

    adding Int and Float extension methods

--- squaretoplusbouncyview/src/main/java/com/anwesh/uiprojects/squaretoplusbouncyview/SquareToPlusBouncyView.kt
+++ squaretoplusbouncyview/src/main/java/com/anwesh/uiprojects/squaretoplusbouncyview/SquareToPlusBouncyView.kt
@@ -19,3 +19,9 @@ val sizeFactor : Float = 2.9f
 val scGap : Float = 0.02f
 val foreColor : Int = Color.parseColor("#4CAF50")
 val backColor : Int = Color.parseColor("#BDBDBD")
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+fun Float.cosify() : Float = 1f - Math.cos(Math.PI / 2 + (this) * (Math.PI / 2)).toFloat()
