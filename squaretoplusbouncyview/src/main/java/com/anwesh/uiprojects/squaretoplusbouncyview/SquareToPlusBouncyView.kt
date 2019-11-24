4f90b8f212d8d65a60beebd3876a126dcd74f310
Anwesh43 <anweshthecool0@gmail.com>
2019-11-24 18:40:24 +0530

    adding SquareToPlusBouncyView class

--- squaretoplusbouncyview/src/main/java/com/anwesh/uiprojects/squaretoplusbouncyview/SquareToPlusBouncyView.kt
+++ squaretoplusbouncyview/src/main/java/com/anwesh/uiprojects/squaretoplusbouncyview/SquareToPlusBouncyView.kt
@@ -55,3 +55,21 @@ fun Canvas.drawVHLNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class SquareToPlusBouncyView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
