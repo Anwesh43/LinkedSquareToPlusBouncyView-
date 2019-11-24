4391709f7649d387d17258ed7450f2fa189be653
Anwesh43 <anweshthecool0@gmail.com>
2019-11-24 19:00:43 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedsquaretoplusbouncyview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedsquaretoplusbouncyview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedsquaretoplusbouncyview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.squaretoplusbouncyview.SquareToPlusBouncyView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         SquareToPlusBouncyView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
