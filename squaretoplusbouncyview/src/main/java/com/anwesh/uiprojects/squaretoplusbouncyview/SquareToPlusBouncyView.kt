53897ba3172b1c2d32bde1f52594116aed31ad81
Anwesh43 <anweshthecool0@gmail.com>
2019-11-24 18:57:10 +0530

    using renderer in view

--- squaretoplusbouncyview/src/main/java/com/anwesh/uiprojects/squaretoplusbouncyview/SquareToPlusBouncyView.kt
+++ squaretoplusbouncyview/src/main/java/com/anwesh/uiprojects/squaretoplusbouncyview/SquareToPlusBouncyView.kt
@@ -60,15 +60,16 @@ fun Canvas.drawSTPBNode(i : Int, scale : Float, paint : Paint) {
 class SquareToPlusBouncyView(ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val renderer : Renderer = Renderer(this)
 
     override fun onDraw(canvas : Canvas) {
-
+        renderer.render(canvas, paint)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
