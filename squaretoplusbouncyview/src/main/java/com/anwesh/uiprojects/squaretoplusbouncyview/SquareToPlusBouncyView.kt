6311c43f21e80f3f8b268b47f75535b145764d38
Anwesh43 <anweshthecool0@gmail.com>
2019-11-24 18:27:07 +0530

    adding constants and libraries

--- squaretoplusbouncyview/src/main/java/com/anwesh/uiprojects/squaretoplusbouncyview/SquareToPlusBouncyView.kt
+++ squaretoplusbouncyview/src/main/java/com/anwesh/uiprojects/squaretoplusbouncyview/SquareToPlusBouncyView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.squaretoplusbouncyview
 /**
  * Created by anweshmishra on 24/11/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+
+val nodes : Int = 5
+val lines : Int = 4
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val scGap : Float = 0.02f
+val foreColor : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#BDBDBD")
